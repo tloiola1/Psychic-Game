3a7e719bc2846ffafae8398a8e4c46635649d00c
Tarciso <tarcisoloiola@hotmail.com>
2017-08-23 23:55:15 -0400

    Final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,49 @@
 document.getElementById("start").addEventListener("click", function(){
+	
 	document.getElementById("start").style.display= "none";
+	document.getElementById("goAhead").style.display= "initial";
+
+	var wins = 0;
+	var losses = 0;
+	var ties = 0;
+
+	document.onkeyup = function(event){
+
+	document.getElementById("goAhead").style.display= "none";
+
+	var userGuess = event.key;
+
+	var pcGuess = String.fromCharCode(Math.floor(Math.random() * 25 + 97));
 
 
-var wins = 0;
-var losses = 0;
-var ties = 0;
+	if (userGuess.toLowerCase() < String.fromCharCode(97) || userGuess.toLowerCase() > String.fromCharCode(122) ||
+		userGuess < String.fromCharCode(97) || userGuess > String.fromCharCode(122)){
 
-document.onkeyup = function(event){
-
-var userGuess = event.key;
-
-var pcGuess = String.fromCharCode(Math.floor(Math.random() * 25 + 97));
-
-
-if (userGuess.toLowerCase() < String.fromCharCode(97) || userGuess.toLowerCase() > String.fromCharCode(122) ||
-	userGuess < String.fromCharCode(97) || userGuess > String.fromCharCode(122)){
-
-	document.getElementById("wrong").style.display= "initial";
-}
-
-else if (userGuess >= String.fromCharCode(97) || userGuess <= String.fromCharCode(122)){
-
-	document.getElementById("wrong").style.display= "none";
-
-	document.getElementById("youChose").innerHTML = userGuess;
-	
-	document.getElementById("pcChose").innerHTML = pcGuess;
-	
-	if(userGuess.toLowerCase() === pcGuess){
-	
-	wins++;
-	document.getElementById("wins").innerHTML = wins;
-	
+		document.getElementById("wrong").style.display= "initial";
 	}
 
-	else {
-	
-	losses++;
-	document.getElementById("losses").innerHTML = losses;
-	
+	else if (userGuess >= String.fromCharCode(97) || userGuess <= String.fromCharCode(122)){
+
+		document.getElementById("wrong").style.display= "none";
+
+		document.getElementById("youChose").innerHTML = userGuess;
+		
+		document.getElementById("pcChose").innerHTML = pcGuess;
+		
+		if(userGuess.toLowerCase() === pcGuess){
+		
+		wins++;
+		document.getElementById("wins").innerHTML = wins;
+		
+		}
+
+		else {
+		
+		losses++;
+		document.getElementById("losses").innerHTML = losses;
+		
+		}
 	}
-}
 
 };
 
